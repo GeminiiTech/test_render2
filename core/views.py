9c1bebf4603a1d18472f76a2a67c808268257a92
Etti Kehinde <116558740+GeminiiTech@users.noreply.github.com>
2025-03-17 15:12:09 +0100

    requirements 4

--- core/views.py
+++ core/views.py
@@ -21,3 +21,20 @@ class NoteView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
+# asgiref==3.8.1
+# dj-database-url==2.3.0
+# Django==5.1.7
+# django-cors-headers==4.7.0
+# djangorestframework==3.15.2
+# djangorestframework_simplejwt==5.5.0
+# gunicorn==23.0.0
+# packaging==24.2
+# psycopg2-binary==2.9.10
+# PyJWT==2.9.0
+# python-dotenv==1.0.1
+# sqlparse==0.5.3
+# typing_extensions==4.12.2
+# tzdata==2025.1
